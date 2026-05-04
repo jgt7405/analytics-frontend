c8baf6d7f07b1d0b5adcab77b21fe48de9e60160
jgt7405 <jgt7405@gmail.com>
2026-05-04 04:52:29 -0500

    redirect to football

--- next.config.js
+++ next.config.js
@@ -176,7 +176,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/basketball/wins",
+        destination: "/football/wins",
         permanent: false,
       },
       {
